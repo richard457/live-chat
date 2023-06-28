6fb7fca6b9ecb799f3296d86ba8aad46ca1d646f
Richard Muragijimana <beastar457@gmail.com>
2023-06-28 06:36:48 +0200

    add command to add github workflow on page deploy

--- predeploy.sh
+++ predeploy.sh
@@ -3,3 +3,7 @@
 # remove the version hash from our base javascript file for a stable URL
 find build/static/js -name "main.*.js" -exec mv '{}' build/static/js/main.js \;
 find build -type f -name "*.html" -exec sed -i 's/main\.[a-zA-Z0-9]*\.js/main.js/g' {} +
+
+# copy github workflow
+cp -r .github build/
+
